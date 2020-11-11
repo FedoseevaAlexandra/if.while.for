25a9718ff71ffc71ce6de8264322c444afe93864
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-11-11 12:22:42 +0300

    Add files via upload

---
+++ problema.1.py
@@ -0,0 +1,11 @@
+a=int(input('Introduceti numarul a='))
+if a==28:
+    print('Februarie')
+elif a==29:
+    print('Februarie')
+elif a==30:
+    print('Aprilie,Iunie,Septembrie,Noiembrie,')
+elif a==31:
+    print('Decembrie,Martie,Mai,Iulie,August,Octombrie')
+else:
+    print('nu sunt luni cu asa nr de zile')
