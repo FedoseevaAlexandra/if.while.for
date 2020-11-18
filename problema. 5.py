cf381cbe478eacafe4419e5b84f1c32e2bec8ebd
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-11-18 19:58:27 +0200

    Add files via upload

---
+++ problema. 5.py
@@ -0,0 +1,26 @@
+an=int(input('introduceti anul '))
+if (an>1000 ) and (an<10000):
+	if an%12==8:
+		print('Anul dragonului')
+	if an%12==9:
+		print('Anul sarpelui')
+	if an%12==10:
+		print('Anul calului')
+	if an%12==11:
+		print('Anul oaiei')
+	if an%12==0:
+		print('Anul maimutei')	
+	if an%12==1:
+		print('Anul cocosului')
+	if an%12==2:
+		print('Anul cainelui')	
+	if an%12==3:
+		print('Anul porcului')
+	if an%12==4:
+		print('Anul sobolanului')
+	if an%12==5:
+		print('Anul boului')
+	if an%12==6:
+		print('Anul tigrului')	
+    if (an%12==7):
+	    print('Anul iepurelui')	
