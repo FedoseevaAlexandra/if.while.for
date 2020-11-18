db4ef4ce24a61ce9f037cbd3a631b065ff528d37
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-11-18 19:56:35 +0200

    Add files via upload

---
+++ problema. 4.py
@@ -0,0 +1,12 @@
+from fractions import Fraction as fr
+import math
+#adunarea a 2 fr date
+a=fr(input('introduceti fractia '))
+b=fr(input('introduceti fractia '))
+s=a+b
+print(s)
+#inmultirea
+a=fr(input('introduceti fractia '))
+b=fr(input('introduceti fractia '))
+i=a*b
+print(i)
