19c9848bf9dbb3389f1630e4feee79529b37e0c8
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-11-19 10:27:26 +0300

    Add files via upload

---
+++ problema.8.py
@@ -0,0 +1,12 @@
+a=float(input('dati nar a '))
+b=float(input('dati nar b '))
+c=float(input('dati nar c '))
+if (a>0) and (b>0) and (c>0):
+    if (a==b) and (b==c):
+        print('triunghi echilateral')
+    if (a**2==b**2+c**2)or(b**2==a**2+c**2)or(c**2==a**2+b**2):
+        print('triunghi dreptunghic')
+    if 
+        print('triunghi scalen')
+else:
+    print("nu pot fi laturile unui triunghi")   
