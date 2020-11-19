d863a00ae47a393b41a6eff8df6f1a2ad8177859
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-11-19 19:47:18 +0200

    Update problema.8.py

--- problema.8.py
+++ problema.8.py
@@ -1,12 +1,12 @@
-a=float(input('dati nar a '))
-b=float(input('dati nar b '))
-c=float(input('dati nar c '))
-if (a>0) and (b>0) and (c>0):
+a=float(input('dati nr a '))
+b=float(input('dati nr b '))
+c=float(input('dati nr c '))
+if (a>0) and(b>0) and(c>0)and(a<b+c) and(b<a+c)and(c<b+a): 
     if (a==b) and (b==c):
         print('triunghi echilateral')
-    if (a**2==b**2+c**2)or(b**2==a**2+c**2)or(c**2==a**2+b**2):
+    elif (a**2==b**2+c**2)or(b**2==a**2+c**2)or(c**2==a**2+b**2):
         print('triunghi dreptunghic')
-    if 
+    else: 
         print('triunghi scalen')
 else:
     print("nu pot fi laturile unui triunghi")   
